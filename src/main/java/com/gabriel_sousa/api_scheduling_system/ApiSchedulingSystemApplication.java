58f771a5e07f8591621dd47448ec9e11b05aa0aa
Gabriel Fernandes De Sousa <95057840+GabrielFDSousa@users.noreply.github.com>
2026-04-28 23:04:51 -0300

    init

---
+++ src/main/java/com/gabriel_sousa/api_scheduling_system/ApiSchedulingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel_sousa.api_scheduling_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSchedulingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSchedulingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabriel_sousa/api_scheduling_system/ApiSchedulingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel_sousa.api_scheduling_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSchedulingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
